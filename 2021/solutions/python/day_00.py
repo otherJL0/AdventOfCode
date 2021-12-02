4e46961f5a8b03f0046e90237b3da418cd15751f
otherJL0 <jonathanglopez@gmail.com>
2021-12-02 11:21:03 -0500

    Updating template to transform input

--- 2021/solutions/python/day_00.py
+++ 2021/solutions/python/day_00.py
@@ -4,6 +4,10 @@ from typing import List
 from aoc import get_input_path, read_input
 
 
+def transform(line: str) -> str:
+    return line
+
+
 def part_one(lines: List[str]):
     pass
 
@@ -15,7 +19,8 @@ def part_two(lines: List[str]):
 def main():
     day: str = sys.argv[0].rstrip(".py").lstrip("day_")
     lines: List[str] = read_input(get_input_path(day))
-    print(lines)
+    formatted_input: List[str] = [transform(line) for line in lines]
+    print(formatted_input)
 
 
 if __name__ == "__main__":
