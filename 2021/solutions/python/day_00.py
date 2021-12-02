7230b27502abb57f4ad714046ef18ac534bdbb8f
otherJL0 <jonathanglopez@gmail.com>
2021-12-02 10:36:22 -0500

    Updating daily template

--- 2021/solutions/python/day_00.py
+++ 2021/solutions/python/day_00.py
@@ -1,11 +1,20 @@
 import sys
+from typing import List
 
 from aoc import get_input_path, read_input
 
 
+def part_one(lines: List[str]):
+    pass
+
+
+def part_two(lines: List[str]):
+    pass
+
+
 def main():
-    day = sys.argv[0].rstrip(".py").lstrip("day_")
-    lines = read_input(get_input_path(day))
+    day: str = sys.argv[0].rstrip(".py").lstrip("day_")
+    lines: List[str] = read_input(get_input_path(day))
     print(lines)
 
 
