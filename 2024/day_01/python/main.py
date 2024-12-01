f8e3fb5599bb6e90565565e303d00a501c3804d8
Jonathan Lopez <jonathanglopez@gmail.com>
2024-12-01 14:02:18 -0500

    Clean up python solution

--- 2024/day_01/python/main.py
+++ 2024/day_01/python/main.py
@@ -5,7 +5,7 @@ from collections import Counter
 
 
 def read_input() -> list[str]:
-    return [line for line in Path("input").read_text().split("\n") if len(line)]
+    return [line for line in Path("../input").read_text().split("\n") if len(line)]
 
 
 def input_to_lists(pairs: list[str]) -> tuple[list[int], list[int]]:
@@ -19,17 +19,22 @@ def input_to_lists(pairs: list[str]) -> tuple[list[int], list[int]]:
     return sorted(left_list), sorted(right_list)
 
 
-def part_one() -> int:
-    left, right = input_to_lists(read_input())
+def part_one(left: list[int], right: list[int]) -> int:
     return reduce(lambda acc, pair: acc + abs(pair[0] - pair[1]), zip(left, right), 0)
 
 
-def part_two() -> int:
-    left, right = input_to_lists(read_input())
+def part_two(left: list[int], right: list[int]) -> int:
     counter = Counter(right)
     return reduce(lambda acc, num: acc + num * counter.get(num, 0), left, 0)
 
 
+def main():
+    left, right = input_to_lists(read_input())
+    result_one = part_one(left, right)
+    print(f"{result_one=}")
+    result_two = part_two(left, right)
+    print(f"{result_two=}")
+
+
 if __name__ == "__main__":
-    print(f"{part_one()=}")
-    print(f"{part_two()=}")
+    main()
