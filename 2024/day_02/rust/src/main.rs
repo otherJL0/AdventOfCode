de48c392bbbae62a15a75b974f525305116390bc
Jonathan Lopez <jonathanglopez@gmail.com>
2024-12-02 12:52:55 -0500

    Adding day 2 part two

--- 2024/day_02/rust/src/main.rs
+++ 2024/day_02/rust/src/main.rs
@@ -20,14 +20,33 @@ fn parse_numbers(report: Vec<String>) -> Vec<Vec<i32>> {
         .collect::<Vec<Vec<i32>>>()
 }
 
+fn is_valid(line: Vec<i32>) -> bool {
+    let deltas = line.windows(2).all(|w| w[0].abs_diff(w[1]) <= 3);
+    let strictly_increasing_or_decreasiing =
+        line.is_sorted_by(|a, b| a < b) || line.is_sorted_by(|a, b| a > b);
+    deltas && strictly_increasing_or_decreasiing
+}
+
 fn part_one(report: Vec<Vec<i32>>) -> usize {
     report
         .into_iter()
+        .map(|line| usize::from(is_valid(line)))
+        .sum()
+}
+
+/// TODO: Find more efficient method
+fn part_two(report: Vec<Vec<i32>>) -> usize {
+    report
+        .into_iter()
         .map(|line| {
-            let deltas = line.windows(2).all(|w| w[0].abs_diff(w[1]) <= 3);
-            let strictly_increasing_or_decreasiing =
-                line.is_sorted_by(|a, b| a < b) || line.is_sorted_by(|a, b| a > b);
-            usize::from(deltas && strictly_increasing_or_decreasiing)
+            for i in 0..line.len() {
+                let mut candidate = line.clone();
+                candidate.remove(i);
+                if is_valid(candidate) {
+                    return 1;
+                }
+            }
+            0
         })
         .sum()
 }
@@ -35,4 +54,6 @@ fn part_one(report: Vec<Vec<i32>>) -> usize {
 fn main() {
     let part_one_result = part_one(parse_numbers(read_input()));
     println!("{part_one_result}");
+    let part_two_result = part_two(parse_numbers(read_input()));
+    println!("{part_two_result}");
 }
